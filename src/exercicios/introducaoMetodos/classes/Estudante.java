bca40d83c44480204f4ba95904b552d6b833b2d0
Pedro Alencar <72420192+alencarable@users.noreply.github.com>
2021-10-19 21:41:27 -0300

    Modificadores de acesso forama dicionados ao exercício Estudante.java

--- src/exercicios/introducaoMetodos/classes/Estudante.java
+++ src/exercicios/introducaoMetodos/classes/Estudante.java
@@ -2,9 +2,9 @@ package exercicios.introducaoMetodos.classes;
 
 public class Estudante {
 
-    public String nome;
-    public int idade;
-    public double[] notas;
+    private String nome;
+    private int idade;
+    private double[] notas;
 
 
     public void imprimir() {
@@ -13,8 +13,8 @@ public class Estudante {
                 this.nome,
                 this.idade);
 
-        if (notas != null) //Se uma nota for esquecida não dará erro (nullpointerexeption)
-        for (double nota : notas) {
+        if (this.notas != null) //Se uma nota for esquecida não dará erro (nullpointerexeption)
+        for (double nota : this.notas) {
             System.out.print(nota + " ");
         }
         System.out.println();
@@ -22,7 +22,7 @@ public class Estudante {
 
     public void media () {
 
-        if (notas == null) {
+        if (this.notas == null) {
             System.out.println("Esse aluno não possui notas");
             return;
         }
@@ -30,16 +30,60 @@ public class Estudante {
         double soma = 0;
 
         for (double nota :
-                notas) {
+                this.notas) {
             soma += nota;
         }
 
-        soma = soma / notas.length;
+        soma = soma / this.notas.length;
 
         if (soma > 6) {
             System.out.printf("A média é: %.2f \nSituação: aprovado!", soma);
         } else {
             System.out.printf("A média é: %.2f \nSituação: reprovado!", soma);
         }
+    }
+
+    //Se o atributo for privado, então deve-se criar um método get e um set.
+
+    /*
+    * Colocar um valor dentro da memória: SET
+    *
+    * Para setar um valor dentro da memória utiliza-se set + nome da variável, seguindo a convenção de código para métodos
+    * Inserir o parâmetro com o tipo e argumento;
+    * Escrever o valor em memória recebendo o argumento do parâmetro.
+    */
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade (int idade) {
+        if (idade < 0) {
+            System.out.println("A idade deve ser maior que zero");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public void setNotas (double[] notas) {
+        this.notas = notas;
+    }
+
+    /*
+    * Buscar um valor dentro da memória: GET
+    *
+    * Utiliza o mesmo tipo de retorno e formatação do método set, sendo que o tipo é passado na construção do método
+    */
+
+    public String getNome () {
+        return this.nome;
+    }
+
+    public int getIdade () {
+        return this.idade;
+    }
+
+    public double[] getNotas () {
+        return this.notas;
     }
 }
--- src/exercicios/introducaoMetodos/testes/EstudanteTeste.java
+++ src/exercicios/introducaoMetodos/testes/EstudanteTeste.java
@@ -8,11 +8,23 @@ public class EstudanteTeste {
 
         Estudante estudante = new Estudante();
 
+        /*
+        Não serão mais usados pois o acesso de Estudantes passou a ser private
+
         estudante.nome = "Pabblo Vittar";
         estudante.idade = 30;
         estudante.notas = new double[] {6, 9, 5.4};
+         */
+
+        estudante.setNome("Pabblo Vittar");
+        estudante.setIdade(30);
+        estudante.setNotas(new double[]{6, 9, 5.4});
 
         estudante.imprimir();
         estudante.media();
+
+        System.out.println(estudante.getNome());
+        System.out.println(estudante.getIdade());
+        System.out.println(estudante.getNotas());
     }
 }
