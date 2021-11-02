9384eddf4aa290090e93d912ea69990df955d05c
Pedro Alencar <72420192+alencarable@users.noreply.github.com>
2021-11-02 18:49:04 -0300

    Aprendendo sobre Blocos de Inicializacão.

---
+++ src/main/javacore/blocoDeInicializacao/classes/Cliente.java
@@ -0,0 +1,34 @@
+package main.javacore.blocoDeInicializacao.classes;
+
+public class Cliente {
+
+    /**
+     1 - Alocado espaço na memória para o objeto que será criado
+     2 - Cada atributo de classe é criado e inicializado com seus valores default ou valores explicitos
+     3 - Bloco de inicialização é executado
+     4 - O constutor é executado
+     */
+
+    private int[] parcelas;
+
+    //Vamos criar o Bloco de Inicialização para colocar as parcelas
+    {
+        parcelas = new int[101];
+        for (int i = 1; i < parcelas.length; i++) {
+            parcelas[i] = i;
+            System.out.println(i);
+        }
+
+    }
+
+    public Cliente () {
+    }
+
+    public int[] getParcelas() {
+        return parcelas;
+    }
+
+    public void setParcelas(int[] parcelas) {
+        this.parcelas = parcelas;
+    }
+}
---
+++ src/main/javacore/blocoDeInicializacao/teste/ClienteTeste.java
@@ -0,0 +1,13 @@
+package main.javacore.blocoDeInicializacao.teste;
+
+import main.javacore.blocoDeInicializacao.classes.Cliente;
+
+public class ClienteTeste {
+
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente();
+
+        System.out.println("Exibindo quantidade de parcelas possíveis");
+    }
+}
