98cd4daa51e29d73da4b24ebc99bc39429a82331
Pedro Alencar <72420192+alencarable@users.noreply.github.com>
2021-11-02 17:52:18 -0300

    Adicionado à Funcionario.java e FuncionarioTeste.java os construtores e suas implicações. Parte 1.

--- src/main/javacore/sobrecargaMetodos/teste/FuncionarioTeste.java
+++ src/main/javacore/sobrecargaMetodos/teste/FuncionarioTeste.java
@@ -6,9 +6,11 @@ public class FuncionarioTeste {
 
     public static void main(String[] args) {
 
-        Funcionario funcionario = new Funcionario();
+        Funcionario funcionario = new Funcionario("Chica da Silva", "111.222.333-44", 3500, "134257890-6");
 
-        funcionario.init("Chica da Silva", "111.222.333-44", 3500, "134257890-6");
+        Funcionario funcionarioVazio = new Funcionario();
+
+        //funcionario.init("Chica da Silva", "111.222.333-44", 3500, "134257890-6");
 
         /*
         funcionario.setNome("Chica da Silva");
@@ -17,5 +19,6 @@ public class FuncionarioTeste {
          */
 
         funcionario.imprime();
+        funcionarioVazio.imprime();
     }
 }
