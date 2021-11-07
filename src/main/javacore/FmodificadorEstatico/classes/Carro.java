ce5994ad751eb45b8573db79de67b4c74ed869a7
Pedro Alencar <72420192+alencarable@users.noreply.github.com>
2021-11-07 20:09:07 -0300

    Aprendendo sobre modificadores estáticos Parte2.

--- src/main/javacore/FmodificadorEstatico/classes/Carro.java
+++ src/main/javacore/FmodificadorEstatico/classes/Carro.java
@@ -1,4 +1,65 @@
 package main.javacore.FmodificadorEstatico.classes;
 
 public class Carro {
+
+    /*
+    Suponhamos que a velocidade limite de todos os carros devem ser 240km/h
+    */
+
+    private String nome;
+    private double velocidadeMaxima;
+
+    /** @static
+     O operador estático faz com que o pertencimento deixe de ser do objeto para a classe.
+     */
+    public static double velocidadeLimite = 240;
+
+    /*
+    * Construtores
+    */
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public Carro() {
+    }
+
+    public void imprime () {
+        System.out.println("Marca: " + this.nome);
+        System.out.println("Velocidade Máxima: " + this.velocidadeMaxima + "km/h");
+        System.out.println("Velocidade Limite: " + velocidadeLimite + "km/h");
+        System.out.println("-----------------");
+    }
+
+    /*
+    * Getters & Setters
+    */
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    /*
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+    */
 }
--- src/main/javacore/FmodificadorEstatico/testes/CarroTeste.java
+++ src/main/javacore/FmodificadorEstatico/testes/CarroTeste.java
@@ -1,4 +1,40 @@
 package main.javacore.FmodificadorEstatico.testes;
 
+import main.javacore.FmodificadorEstatico.classes.Carro;
+
 public class CarroTeste {
+
+    public static void main(String[] args) {
+
+        Carro esportivo = new Carro("BMW", 280);
+        Carro utilitario = new Carro("Audi", 275);
+        Carro luxo = new Carro("Mercedes", 300);
+
+        esportivo.imprime();
+        utilitario.imprime();
+        luxo.imprime();
+
+        System.out.println("##################");
+
+        /*
+        Ao alterar a velocidade limite, no caso um atributo estático, mesmo alterando apenas um,
+        todos os outros serão afetados, pois o atributo estático referencia à classe e não ao objeto.
+
+
+        esportivo.setVelocidadeLimite(200);
+        esportivo.imprime();
+        utilitario.imprime();
+        luxo.imprime();
+
+         */
+
+        /**
+        O certo seria
+
+        Carro.velocidadeLimite = 200
+
+         excluindo-se os getters & setters desse atributo.
+        */
+
+    }
 }
