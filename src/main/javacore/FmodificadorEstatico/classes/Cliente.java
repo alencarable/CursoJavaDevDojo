a39727b2b46dbf6331a6dab539c6e51bf7ea859b
Pedro Alencar <72420192+alencarable@users.noreply.github.com>
2021-11-07 21:10:09 -0300

    Aprendendo sobre bloco de inicialização estático.

--- src/main/javacore/FmodificadorEstatico/classes/Cliente.java
+++ src/main/javacore/FmodificadorEstatico/classes/Cliente.java
@@ -3,7 +3,7 @@ package main.javacore.FmodificadorEstatico.classes;
 public class Cliente {
 
     /**
-     1 - Bloco de inicialização é executado quando a JVM carregar a classe (é executado apenas 1 vez)
+     1 - Bloco de inicialização estático é executado quando a JVM carregar a classe (é executado apenas 1 vez)
      2 - Alocado espaço na memória para o objeto que será criado
      3 - Cada atributo de classe é criado e inicializado com seus valores default ou valores explicitos
      4 - Bloco de inicialização é executado
